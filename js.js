35a9fa3c1e5554f9472228a3c194fc387057c34a
ebrahim78652 <ebrahimkarjatwala@gmail.com>
2022-03-29 23:07:51 +0200

    tying to get image to work on github pages

---
+++ js.js
@@ -0,0 +1,6 @@
+var confirm_password= document.getElementById('input-confirm_password')
+console.log(confirm_password);
+
+confirm_password.addEventListener(onkeyup, ()=>{
+    console.log("hi");
+} )
